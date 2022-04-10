23a9fdd69e5b7baa98abd3973f562ada7822dc64
avdivo <avdivo@mail.ru>
2022-04-11 00:09:04 +0300

    Закончена фильтрация и сортировка

--- shop/shop/views.py
+++ shop/shop/views.py
@@ -14,7 +14,9 @@ def about(request):
     return render(request, 'about.html', locals())
 
 # Магазин (выбор товара)
-def shop(request, value=None):
+
+
+def shop(request, filter=' '):
     # Сортировка проверяется в сессии и применяется при каждом выводе товаров
     # По умолчанию производится сортировка по Популярности товаров
     # Смена сортировки приходит методом POST в параметре sort
@@ -36,27 +38,28 @@ def shop(request, value=None):
     # Сортировка
     # Если сортировка не установлена, то устанавливаем ее по умолчанию
     if 'sort' in request.session:
-        print(request.session['sort'])
+        sort = request.session['sort']
     else:
         request.session['sort'] = '-popular'
-    sort = request.session['sort']
-    # Gymtast.objects.all().exclude(rank=None).order_by("-rank")
+    if 'sort' in request.POST:
+        # Получение данных о сортировке методом POST
+        sort = request.POST['sort']
+    request.session['sort'] = sort
 
     products = []
-    if value and re.search(r"[<>%\$]$", value) == None:
+    if filter != ' ' and re.search(r"[<>%\$]$", filter) == None:
         # Параметр есть и он допустимый
-        if len(Department.objects.filter(name=value)):
+        if len(Department.objects.filter(alias=filter)):
             # Фильтрация по Отделам
-            products = Product.objects.filter(category__department__alias=value).order_by(sort)
-        if len(products) == 0 and len(Category.objects.filter(alias=value)):
+            products = Product.objects.filter(category__department__alias=filter).order_by(sort)
+        if len(products) == 0 and len(Category.objects.filter(alias=filter)):
             # Фильтрация по Категориям
-            products = Product.objects.filter(category__alias=value).order_by('-popular')
-        if len(products) == 0 and len(Holiday.objects.filter(alias=value)):
+            products = Product.objects.filter(category__alias=filter).order_by(sort)
+        if len(products) == 0 and len(Holiday.objects.filter(alias=filter)):
             # Фильтрация по Подаркам
-            products = Product.objects.filter(holiday__alias=value)
-
+            products = Product.objects.filter(holiday__alias=filter).order_by(sort)
     else:
-        products = Product.objects.order_by('-popular')
+        products = Product.objects.order_by(sort)
 
     # Выбор главной фотографии для каждого товара
     page_obj = []
@@ -68,6 +71,7 @@ def shop(request, value=None):
             product.photo = photo[0].image.url
         page_obj.append(product)
 
+    # sort и filter передаются в шаблон
     return render(request, 'shop.html', locals())
 
 # Контакты
