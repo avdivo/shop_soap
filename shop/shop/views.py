d4183333e2ba15b21e3f96dcfb846f938d420e22
Alexey <avdivo@mail.ru>
2022-04-11 17:58:17 +0300

    Делаю поиск

--- shop/shop/views.py
+++ shop/shop/views.py
@@ -2,28 +2,33 @@ from django.shortcuts import render
 from django.conf import settings
 from order.models import Order
 from product.models import *
+from django.db.models import Q
 
 # Главная страница
 def index(request):
-    ord = {'orders':Order.objects.all()}
+    ord = {'orders': Order.objects.all()}
     return render(request, 'index.html', locals())
+
 
 # О магазине
 def about(request):
-
     return render(request, 'about.html', locals())
+
 
 # Магазин (выбор товара)
 
 
-def shop(request, filter=' '):
+def shop(request, filter=None):
     # Сортировка проверяется в сессии и применяется при каждом выводе товаров
     # По умолчанию производится сортировка по Популярности товаров
     # Смена сортировки приходит методом POST в параметре sort
 
+    # Если метод POST принял параметр search производится
+    # Регистронезависимый поиск вхождения  Артиклах, Названиях, Описании товаров
+    # Если ничего не найдено выводится сообщение об этом
+
     # Функция может получать значение для фильтрации и выбирает нужные товары, по умолчанию все
     # Если знаение есть, оно ищется в Отделах, Категориях, Праздниках
-    # Если не найдено ищется в Артиклах, Названиях, Описании товаров
 
     # После делается пангинация
     # Затем добавляются главные фотографии к товарам
@@ -37,34 +42,45 @@ def shop(request, filter=' '):
 
     # Сортировка
     # Если сортировка не установлена, то устанавливаем ее по умолчанию
-    if 'sort' in request.session:
-        sort = request.session['sort']
-    else:
+    if not 'sort' in request.session:
         request.session['sort'] = '-popular'
+    sort = request.session['sort']
     if 'sort' in request.POST:
         # Получение данных о сортировке методом POST
         sort = request.POST['sort']
     request.session['sort'] = sort
 
     products = []
-    if filter != ' ' and re.search(r"[<>%\$]$", filter) == None:
-        # Параметр есть и он допустимый
-        if len(Department.objects.filter(alias=filter)):
-            # Фильтрация по Отделам
-            products = Product.objects.filter(category__department__alias=filter).order_by(sort)
-        if len(products) == 0 and len(Category.objects.filter(alias=filter)):
-            # Фильтрация по Категориям
-            products = Product.objects.filter(category__alias=filter).order_by(sort)
-        if len(products) == 0 and len(Holiday.objects.filter(alias=filter)):
-            # Фильтрация по Подаркам
-            products = Product.objects.filter(holiday__alias=filter).order_by(sort)
+    # Поиск
+    if 'search' in request.POST:
+        # Получение данных о поиске методом POST, проверка на недопустимые символы и поиск
+        search = request.POST['search']
+        if re.search(r"[<>%\$]$", search) == None:
+            # products = Product.objects.filter(article__icontains=search)Q(name__icontains=search)Q
+            # (description__icontains=search).order_by(sort)
+            # products = Product.objects.annotate(search = SearchVector('article', 'name', 'description_')).filter(active=True)
+            # products = Product.objects.filter(Q(article__contains=search) | Q(name__contains=search))
+            products = Product.objects.filter(name__icontains=search, active=True).order_by(sort)
+
     else:
-        products = Product.objects.order_by(sort)
+        if filter != None and re.search(r"[<>%\$]$", filter) == None:
+            # Параметр есть и он допустимый
+            # Фильтрация по Отделам
+            products = Product.objects.filter(category__department__alias=filter, active=True).order_by(sort)
+            if len(products) == 0:
+                # Фильтрация по Категориям
+                products = Product.objects.filter(category__alias=filter, active=True).order_by(sort)
+            if len(products) == 0:
+                # Фильтрация по Подаркам
+                products = Product.objects.filter(holiday__alias=filter, active=True).order_by(sort)
+        else:
+            products = Product.objects.filter(active=True).order_by(sort)
 
     # Выбор главной фотографии для каждого товара
     page_obj = []
     for product in products:
-        photo = ProductImage.objects.filter(product_id=product.id, active=True, main=True) # Выбираем главную фотографию для товара
+        photo = ProductImage.objects.filter(product_id=product.id, active=True,
+                                            main=True)  # Выбираем главную фотографию для товара
         # Если нет главной фотографии для товара подставляем заглушку. Добавляем url как свойство Товара
         product.photo = settings.NO_PHOTO
         if len(photo):
@@ -74,12 +90,12 @@ def shop(request, filter=' '):
     # sort и filter передаются в шаблон
     return render(request, 'shop.html', locals())
 
+
 # Контакты
 def contact(request):
-
     return render(request, 'contact.html', locals())
+
 
 # Отдельный товар
 def shop_single(request):
-
     return render(request, 'shop-single.html', locals())
