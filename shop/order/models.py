ab7de527cdd34518d9639a103c8e03adecec6f9a
avdivo <avdivo@mail.ru>
2022-04-21 00:25:14 +0300

    Изменение моделей для осуществления покупок без регистрации. Добавление дмодели с альтернативной информацией о заказчике

--- shop/order/models.py
+++ shop/order/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save, post_delete
 from django.conf import settings
 from product.models import Product
 from django.utils.timezone import now
+from django.core.validators import RegexValidator
 
 
 # Статусы заказов
@@ -30,7 +31,6 @@ class DeliveryMethod(models.Model):
         verbose_name = 'Метод доставки'
         verbose_name_plural = 'Методы доставки'
 
-
 # Заказы
 class Order(models.Model):
     number = models.CharField(max_length=8, blank=True, default=None, verbose_name=u'Номер заказа')  # Номер заказа
@@ -41,7 +41,7 @@ class Order(models.Model):
     status = models.ForeignKey(StatusOrder, on_delete=models.PROTECT, default=1,
                                verbose_name=u'Статус')  # Связь с таблицей статусов
     delivery_method = models.ForeignKey(DeliveryMethod, on_delete=models.PROTECT, default=1,
-                                        verbose_name=u'Метод доставки')  # Связь с таблицей статусов
+                                        verbose_name=u'Метод доставки')  # Связь с таблицей методов доставки
     address_delivery = models.CharField(max_length=256, blank=True, default=None, null=True,
                                         verbose_name=u'Адрес доставки')  # Адрес доставки
     price_product = models.IntegerField(default=0,
@@ -53,6 +53,8 @@ class Order(models.Model):
                                    verbose_name=u'Дата создания')  # Давта создания заказа
     updated = models.DateTimeField(default=now,
                                    verbose_name=u'Дата изменения')  # Давта изменения заказа
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=True, null=True, verbose_name=u'Заказчик')  # Ссылка на таблицу с User
+
 
     def __str__(self):
         return str(self.id)
@@ -69,7 +71,6 @@ class Order(models.Model):
         self.price_total = self.price_product + self.price_delivery
         if self.price_individual: self.price_total = self.price_individual
         super(Order, self).save(*args, **kwargs)
-
 
 
 # Обработка сигнала оохранения Заказа для формирования и записи Номера заказа
@@ -121,3 +122,27 @@ def post_save_ProductInOrder(sender, instance, **kwargs):
 # Для модели товаров в заказе
 post_save.connect(post_save_ProductInOrder, sender=ProductInOrder)  # Сигнал после сохранения
 post_delete.connect(post_save_ProductInOrder, sender=ProductInOrder)  # Сигнал после удаления
+
+
+# Альтернативный профиль для заказа
+# Данные профиля пользователя покупающего без регистрации или зарегистрированного,
+# но пожелавшего изменить данные для заказа
+class AlternateProfile(models.Model):
+    first_name = models.CharField(max_length=64, verbose_name=u'Имя')  # Имя
+    last_name = models.CharField(max_length=64, verbose_name=u'Фамилия')  # Фамилия
+    patronymic = models.CharField(max_length=64, blank=True, verbose_name=u'Отчество')  # Отчество
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phoneNumber = models.CharField(validators=[phoneNumberRegex], max_length=16,
+                                   verbose_name=u'Телефон')  # Номер телефона
+    address = models.CharField(max_length=128, verbose_name=u'Адрес доставки')  # Адрес
+    email = models.EmailField(max_length=150)
+    # order = models.OneToOneField(Order, on_delete=models.CASCADE, blank=True, null=True, verbose_name=u'Заказчик')   # Ссылка на таблицу с Заказом
+
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    # Как писать назнвние в единственном и множественном числе
+    class Meta:
+        verbose_name = 'Альтернативные данные'
+        verbose_name_plural = 'Альтернативные данные'
