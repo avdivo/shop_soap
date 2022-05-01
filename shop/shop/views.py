7836bfe4f0b74e78b54e3ea27752e477ae985ac7
avdivo <avdivo@mail.ru>
2022-05-01 18:28:48 +0300

    Личный кабинет, работа над разделом Заказы

--- shop/shop/views.py
+++ shop/shop/views.py
@@ -415,7 +415,6 @@ def order_accept(request, order=None):
         return redirect('index')
     order = request.session['order']
     is_email = request.session['is_email']
-    print(is_email)
     return render(request, 'order_accept.html', locals())
 
 # Регистрация нового пользователя ---------------------------------------------------
@@ -495,25 +494,41 @@ class ProfilePage(TemplateView):
             chapter = kwargs['data']
 
         if request.user.is_authenticated:
-            # Проверяем, есьб ли куда вернуться, если нет, назначаем
+            # Проверяем, есть ли куда вернуться, если нет, назначаем
             if 'return' not in request.session:
                 request.session['return'] = 'index'
 
+            # Подготовка и работа с Профилем пользователя
             profile_data = Profile.objects.get(user=request.user)
             profile = profile_data.get_user_data()
 
-            if request.method == 'POST':
-                # Сохраняем данные пользователя не проверяя
-                profile = request.POST
-                profile_data.patronymic = request.POST.get("patronymic")
-                profile_data.phoneNumber = request.POST.get("phoneNumber")
-                profile_data.address = request.POST.get("address")
-                profile_data.save()
-                request.user.first_name = request.POST.get("first_name")
-                request.user.last_name = request.POST.get("last_name")
-                request.user.save()
+            if chapter == 'profile':
 
-                return redirect(request.session['return'])
+                if request.method == 'POST':
+                    # Сохраняем данные пользователя не проверяя
+                    profile = request.POST
+                    profile_data.patronymic = request.POST.get("patronymic")
+                    profile_data.phoneNumber = request.POST.get("phoneNumber")
+                    profile_data.address = request.POST.get("address")
+                    profile_data.save()
+                    request.user.first_name = request.POST.get("first_name")
+                    request.user.last_name = request.POST.get("last_name")
+                    request.user.save()
+
+                    return redirect(request.session['return'])
+            else:
+                # Подготовка страницы с Заказами пользователя
+                orders = Order.objects.filter(user=request.user) # Заказы пользователя
+
+                for order in orders:
+                    # Товары в заказах добавляем в свойство заказа
+                    order.products = ProductInOrder.objects.filter(order=order)
+                    # Исправляем даты и время на нужный формат
+                    offset = datetime.timedelta(hours=3)  # Исправляем время для правильного отображения
+                    order.created += offset
+                    order.created = order.created.strftime("%d.%m.%Y %H:%M")
+                    order.updated += offset
+                    order.updated = order.updated.strftime("%d.%m.%Y %H:%M")
 
         else:
             return redirect(reverse("login"))
