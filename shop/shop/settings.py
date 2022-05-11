c48ccd4def59ae850ee5570352c8a97f40d3f850
Alexey <avdivo@mail.ru>
2022-05-11 18:02:11 +0300

    Деплой

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-em0_tpxfru1&v!r_xkkyqlgu!c!-=b6-o-0fnuq5xjbn4)6at+'
+# SECRET_KEY = 'django-insecure-em0_tpxfru1&v!r_xkkyqlgu!c!-=b6-o-0fnuq5xjbn4)6at+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-em0_tpxfru1&v!r_xkkyqlgu!c!-=b6-o-0fnuq5xjbn4)6at+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['green-soap.ru']
-
+ALLOWED_HOSTS = [] if DEBUG else ['green-soap.ru']
 
 # Application definition
 
